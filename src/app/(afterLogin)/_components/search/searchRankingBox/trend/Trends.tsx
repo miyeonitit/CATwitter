fe7402cdac83d7152d68741036a053b11c188608
miyeonitit <rrpec9@gmail.com>
2024-02-26 17:02:21 +0900

    🏗️ WIP: add Trends components 2

--- src/app/(afterLogin)/_components/search/searchRankingBox/trend/Trends.tsx
+++ src/app/(afterLogin)/_components/search/searchRankingBox/trend/Trends.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { HashTag } from "@/interfaces/HashTag";
 
 import styles from "./Trends.module.css";
@@ -8,13 +10,13 @@ type TrendProps = {
 
 const Trends = ({ trend }: TrendProps) => {
   return (
-    <>
+    <Link href={`/search?q=${trend.title}`}>
       <div className={styles.trending_tag}>Trending in South Korea</div>
       <div className={styles.trending_words}>{trend.title}</div>
       <div className={styles.trending_tag}>
         {trend.count.toLocaleString()}K posts
       </div>
-    </>
+    </Link>
   );
 };
 
